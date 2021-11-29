0c6d143826f9ba7f77784833811109d220488114
ShaunLWM <shaunlimweimin@gmail.com>
2021-11-29 20:05:59 +0800

    Fix metamask chain detection

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -70,6 +70,7 @@ const config: HardhatUserConfig = {
       live: false,
       saveDeployments: true,
       tags: ["test", "local"],
+      chainId: 1337,
     },
     ropsten: {
       url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
